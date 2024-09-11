c80a381bb9548ce86fa98a38928c1c6c6395d6ee
Nakhba Mubashir <epl482@usask.ca>
2024-09-11 17:12:37 -0600

    wrote the bash script

--- lab1/run-lab1.bash
+++ lab1/run-lab1.bash
@@ -3,4 +3,20 @@
 #NAKHBA MUBASHIR
 #epl482
 #11317060
-
+if [ $# -ne 0 ]
+then
+	echo "ERROR: not enough arguments"
+else
+	echo "the machine hardware architecture: $(uname -m)"
+	echo "excetuting"
+	if [ $(uname -m) = "ppc" ]
+	then
+		
+	elif [ $(uname -m) == "arm" ]
+		
+	else
+		
+	fi
+	echo "finished excecuting"
+fi
+		
