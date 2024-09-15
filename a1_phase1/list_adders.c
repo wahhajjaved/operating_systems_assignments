cf05fec246fa58751cdd0535355436ad8d9f878d
Nakhba Mubashir <epl482@usask.ca>
2024-09-14 19:39:35 -0600

    added the checks for correct type and correct parameter

---
+++ a1_phase1/list_adders.c
@@ -0,0 +1,95 @@
+/* Authors
+ *Wahhaj Javed, muj975, 11135711
+ *Nakhba Mubashir, epl482, 11317060
+ *
+ */
+
+#include "list.h"
+
+
+int ListAdd(LIST *list,void *item){
+     // if the List struct pointer is not right
+    if (list == NULL) {
+        printf("ERROR: list is NULL \n");
+        return -1;
+    }
+    // check if the list size is less than max, or node is less than max
+    if (list-> size <MAX_LIST_SIZE || nodes_num > MAX_NODE ){
+        printf("ERROR: list is full or max number of nodes have been reached");
+        return NULL;
+    if (item == NULL){
+        printf("ERROR: item is NULL");
+        return -1;
+    }
+
+   /*TODO */
+    return -1;
+
+}
+int ListInsert(LIST *list,void *item){
+     // if the List struct pointer is not right
+    if (list == NULL) {
+        printf("ERROR: list is NULL \n");
+        return -1;
+    }
+    // check if the list size is less than max, or node is less than max
+    if (list-> size <MAX_LIST_SIZE || nodes_num > MAX_NODE ){
+        printf("ERROR: list is full or max number of nodes have been reached");
+        return NULL;
+    if (item == NULL){
+        printf("ERROR: item is NULL");
+        return -1;
+    }
+
+   /*TODO */
+    return -1;
+
+}
+
+int ListAppend(LIST *list, void *item){
+     // if the List struct pointer is not right
+    if (list == NULL) {
+        printf("ERROR: list is NULL \n");
+        return -1;
+    }
+    // check if the list size is less than max, or node is less than max
+    if (list-> size <MAX_LIST_SIZE || nodes_num > MAX_NODE ){
+        printf("ERROR: list is full or max number of nodes have been reached");
+        return NULL;
+    if (item == NULL){
+        printf("ERROR: item is NULL");
+        return -1;
+    }
+
+   /*TODO */
+    return -1;
+
+}
+int ListPrepend(LIST *list,void *item){
+     // if the List struct pointer is not right
+    if (list == NULL) {
+        printf("ERROR: list is NULL \n");
+        return -1;
+    }
+    // check if the list size is less than max, or node is less than max
+    if (list-> size <MAX_LIST_SIZE || nodes_num > MAX_NODE ){
+        printf("ERROR: list is full or max number of nodes have been reached");
+        return NULL;
+    if (item == NULL){
+        printf("ERROR: item is NULL");
+        return -1;
+    }
+
+   /*TODO */
+    return -1;
+}
+
+void ListConcat(LIST *list1, LIST *list2){
+     // if the List struct pointer is not right
+    if (list1 == NULL || list2== NULL) {
+        printf("ERROR: list1 OR list2 is NULL \n");
+        return NULL;
+    }
+   /*TODO */
+    return NULL
+}
