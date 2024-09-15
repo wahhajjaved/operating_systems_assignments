bf8cc01b015ba233f5f87c45da792e2514e86944
Nakhba Mubashir <epl482@usask.ca>
2024-09-15 00:13:37 -0600

    implemented according to the instruction

---
+++ a1_phase1/mytestlist.c
@@ -0,0 +1,63 @@
+/* Authors
+ *Wahhaj Javed, muj975, 11135711
+ *Nakhba Mubashir, epl482, 11317060
+ *
+ */
+#include"list.h"
+
+int main(void){
+    LIST *L1;
+    LIST *L2;
+    void *I1;
+
+    L1= ListCreat();
+	printf("got to procedure ListCreate()\n");
+
+	ListCount(L1);
+	printf("got to procedure ListCount()\n");
+
+	I1 = ListFirst(L1);
+	printf("got to procedure ListFirst()\n");
+
+	I1 = ListLast(L1);
+	printf("got to procedure ListLast()\n");
+
+	I1 = ListNext(L1);
+	printf("got to procedure ListNext()\n");
+
+	I1 = ListPrev(L1);
+	printf("got to procedure ListPrev()\n");
+
+	I1 = ListCurr(L1);
+	printf("got to procedure ListCurr()\n");
+	
+	ListAdd(L1, I1);
+	printf("got to procedure ListAdd()\n");
+
+	ListInsert(L1, I1);
+	printf("got to procedure ListInsert()\n");
+	
+	ListAppend(L1, I1);
+	printf("got to procedure ListAppend()\n");
+
+	ListPrepend(L1, I1);
+	printf("got to procedure ListPrepend()\n");
+
+	I1 = ListRemove(L1);
+	printf("got to procedure ListRemove()\n");
+
+	ListConcat(L1, L2);
+	printf("got to procedure ListConcat()\n");
+
+	I1 = ListTrim(L1);
+	printf("got to procedure ListTrim()\n");
+
+	ListFree(L2, &TestItemFree);
+	printf("got to procedure ListFree()\n");
+
+	I1 = ListSearch(L1, &TestComparator, I1);
+	printf("got to procedure ListSearch()\n");
+
+	return 0;
+
+}
