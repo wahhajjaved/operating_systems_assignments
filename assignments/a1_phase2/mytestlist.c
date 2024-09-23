39ad89bba73357219b4818bb7a11e70688597514
Nakhba Mubashir <epl482@usask.ca>
2024-09-22 18:17:53 -0600

    changed my print list function because it was printing core dump before

--- assignments/a1_phase2/mytestlist.c
+++ assignments/a1_phase2/mytestlist.c
@@ -32,10 +32,14 @@ int main(void){
 
     printf("______ListCreate()________\n");
     L1= ListCreate();
+    Print_List(L1);
     L2= ListCreate();
+    Print_List(L2);
     L3= ListCreate();
+    Print_List(L3);
     L4= ListCreate();
-
+    Print_List(L4);
+    
 /*
 	ListCount(L1);
 	printf("got to procedure ListCount()\n");
@@ -59,12 +63,13 @@ int main(void){
     printf("________ListADD()___________\n");
     printf("Empty list()\n");
     result=ListAdd(L1, I1);
+/*    Print_List(L1);*/
     if (result !=0){
         printf("ERROR : listAdd.\n");
         printf("list1=");
         Print_List(L1);
     }
-    else printf("ListAdd() successful!!");
+    else printf("ListAdd() successful!!\n");
 
     printf("NON Empty lis()\n");
     result=ListAppend(L1, I5);
@@ -73,10 +78,8 @@ int main(void){
         printf("list1=");
         Print_List(L1);
     }
-    else printf("ListAdd() successful!!");
-
-	ListInsert(L1, I1);
-	printf("got to procedure ListInsert()\n");
+    else printf("ListAdd() successful!!\n");
+    Print_List(L1);
 
     printf("________ListInsert()___________\n");
     printf("Empty list()\n");
@@ -86,7 +89,7 @@ int main(void){
         printf("list2=");
         Print_List(L2);
     }
-    else printf("ListInsert() successful!!");
+    else printf("ListInsert() successful!!\n");
 
     printf("NON Empty lis()\n");
     result=ListInsert(L2, I6);
@@ -95,8 +98,8 @@ int main(void){
         printf("list2=");
         Print_List(L2);
     }
-    else printf("ListInsert() successful!!");
-
+    else printf("ListInsert() successful!!\n");
+    Print_List(L2);
 	
 
     printf("________ListAppend()___________\n");
@@ -107,7 +110,7 @@ int main(void){
         printf("list3=");
         Print_List(L3);
     }
-    else printf("ListAppend() successful!!");
+    else printf("ListAppend() successful!!\n");
 
     printf("NON Empty lis()\n");
     result=ListAppend(L3, I2);
@@ -116,8 +119,8 @@ int main(void){
         printf("list3=");
         Print_List(L3);
     }
-    else printf("ListAppend() successful!!");
-    
+    else printf("ListAppend() successful!!\n");
+    Print_List(L3);    
 
     printf("________ListPrepend()___________\n");
     printf("Empty lis()\n");
@@ -127,7 +130,7 @@ int main(void){
         printf("list4=");
         Print_List(L4);
     }
-    else printf("ListPrepend() successful!!");
+    else printf("ListPrepend() successful!!\n");
 
     printf("NON Empty lis()\n");
     result=ListAppend(L4, I2);
@@ -136,7 +139,9 @@ int main(void){
         printf("list4=");
         Print_List(L4);
     }
-    else printf("ListPrepend() successful!!");
+    else printf("ListPrepend() successful!!\n");
+    Print_List(L4);
+
 /*
 	I1 = ListRemove(L1);
 	printf("got to procedure ListRemove()\n");
@@ -158,12 +163,12 @@ int main(void){
 }
 /* print list for testing*/
 void Print_List(LIST *list){
-    int *item;
+    NODE *printing_node= list->head;
     printf("[");
     if (ListCount(list)>0){
-        printf("%d", *((int*) ListFirst(list)));
-        while ((item =ListNext(list)) != NULL) {
-            printf (", %d" , *item);
+        while (printing_node != NULL){
+            printf("%d ", *((int*) printing_node->item ));
+            printing_node= printing_node->next;
         }
     }
     printf("]\n");
