9659b7d2bb2551bf3cb065810e55ed9271136e21
Wahhaj Javed <muj975@usask.ca>
2024-09-29 15:19:38 -0600

    pingpong.c hello world created. Compiles and runs

--- assignments/xv6/Makefile
+++ assignments/xv6/Makefile
@@ -139,6 +139,7 @@ UPROGS=\
 	$U/_grind\
 	$U/_wc\
 	$U/_zombie\
+	$U/_pingpong\
 
 fs.img: mkfs/mkfs README $(UPROGS)
 	mkfs/mkfs fs.img README $(UPROGS)
---
+++ assignments/xv6/user/pingpong.c
@@ -0,0 +1,11 @@
+/*
+* @author Wahhaj Javed, muj975, 11135711
+* @author Nakhba Mubashir, epl482, 11317060
+* @date 2024-09-22
+*/
+
+#include <stdio.h>
+int main(int argc, char* argv[]) {
+    printf("Hello world\n");
+    return 0;
+}
