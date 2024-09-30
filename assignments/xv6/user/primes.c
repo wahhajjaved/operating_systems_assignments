661e73b89041c0c599887e8f8aa6d3506a2bf85c
Wahhaj Javed <muj975@usask.ca>
2024-09-30 11:25:07 -0600

    part D bonus hello world compiles and runs

---
+++ assignments/xv6/user/primes.c
@@ -0,0 +1,13 @@
+/*
+* @author Wahhaj Javed, muj975, 11135711
+* @author Nakhba Mubashir, epl482, 11317060
+* @date 2024-09-30
+*/
+
+#include "kernel/types.h"
+#include "user/user.h"
+
+int main(int argc, char* argv[]) {
+	printf("Hello world\n");
+	return 0;
+}
