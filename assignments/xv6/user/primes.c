4828211e6e331ab913dbb44dc2d7dce1b8fe85bf
Wahhaj Javed <muj975@usask.ca>
2024-10-02 15:14:50 -0600

    Primes compiles, run, and terminates. Output not correct

--- assignments/xv6/user/primes.c
+++ assignments/xv6/user/primes.c
@@ -7,95 +7,201 @@
 #include "kernel/types.h"
 #include "user/user.h"
 
-#define MAX_INT 5
+#define MAX_INT 3
 
 int pipe1[2]; //incoming
 int pipe2[2]; //outgoing
 int incoming = -1;
 int outgoing = -1;
 
+void print_state() {
+	printf("%d: pipe 1 = {%d, %d}, pipe 2 = {%d, %d}, "
+		"incoming = %d, outgoing = %d\n", 
+		getpid(), pipe1[0], pipe1[1], pipe2[0], pipe2[1], incoming, outgoing
+	);
+	
+}
+
 int createProcess() {
 	int childPid;
+	
+	printf("%d: CreateProcess() start.\n", getpid());
+	print_state();
+	
+	
+	sleep(1);
 	if (pipe2[0] != -1 && pipe2[1] != -1) {
-		printf("%d: Can not create process as pipe2 is already initialized\n", getpid());
+		printf("%d: Can not create process as pipe2 is already initialized.\n", 
+			getpid()
+		);
 		return -1;
 	}
 	
+	sleep(1);
 	if (pipe(pipe2) == -1) {
 		printf("%d: Could not create pipe2 pipe.\n", getpid());
 		return -1;
 	}
+	
+	printf("%d: pipe2 created in createProcess().\n", getpid());
+	print_state();
+	printf("\n");
+	sleep(1);
 
 	childPid = fork();
+	sleep(1);
 	if (childPid == -1) {
 		printf("%d: fork failed in createProcess().\n", getpid());
 		return -1;
 	}
 	else if (childPid == 0) {
-		incoming = pipe2[0];
-		outgoing = -1;
-		if(close(pipe1[0]) != 0) {
-			printf("%d: Error in createProcess. Could not close pipe1[0]\n", getpid());
-			return -1;
-		}
-		pipe1[0] = -1;
+		// close pipe1 because it connects to the parent's parent
+		sleep(3);
+		printf("\n");
+		printf("%d: createProcess() in child after fork.\n", getpid());
+		print_state();
 		
-		if(close(pipe2[1]) != 0) {
-			printf("%d: Error in createProcess. Could not close pipe2[1]\n", getpid());
+		if(close(pipe1[0]) != 0) {
+			printf("%d: Error in createProcess. Could not close pipe1[0]\n", 
+				getpid()
+			);
 			return -1;
 		}
+		// if(close(pipe1[1]) != 0) {
+		// 	printf("%d: Error in createProcess. Could not close pipe1[1]\n", 
+		// 		getpid()
+		// 	);
+		// 	return -1;
+		// }
+
+		sleep(3);
+		if(close(pipe2[1]) != 0) {
+			printf("%d: Error in createProcess. Could not close pipe2[1]\n", 
+				getpid()
+			);
+			return -1;
+		}
+		
+		pipe1[0] = pipe2[0];
+		// pipe1[1] = -1;
+		pipe2[0] = -1;
 		pipe2[1] = -1;
+		sleep(3);
+		printf("%d: createProcess() in child end.\n", getpid());
+		print_state();
+		sleep(3);
 	}
 	else {
-		outgoing = pipe2[1];
+		sleep(1);
+		printf("%d: createProcess() after fork in parent.\n", getpid());
+		print_state();
+		
+		// pipe1 = [3, 4]
+		// pipe2 = [5, 6]
+		
+		// if(close(pipe1[1]) != 0) {
+		// 	printf("%d: Error in createProcess. Could not close pipe1[1]\n", 
+		// 		getpid()
+		// 	);
+		// 	return -1;
+		// }
 		if(close(pipe2[0]) != 0) {
-			printf("%d: Error in createProcess. Could not close pipe2[0]\n", getpid());
+			printf("%d: Error in createProcess. Could not close pipe2[0]\n", 
+				getpid()
+			);
 			return -1;
 		}
+		
+		// pipe1[1] = -1;
 		pipe2[0] = -1;
+		
+		sleep(1);
+		printf("%d: createProcess() in parent end.\n", getpid());
+		print_state();
 	}
+	
+	sleep(2);
+	incoming = pipe1[0];
+	outgoing = pipe2[1];
+	printf("%d: createProcess() end.\n", getpid());
+	print_state();
 	return childPid;
 	
 }
 
 void prime() {
-	int divisor, nextInt, bytesRead;
-	// int childPid, divisor, nextInt, outgoing, incoming;
+	// int divisor, nextInt, bytesRead;
+	int childPid, divisor, number, bytesRead;
+	childPid = -1;
 	divisor = -1;
+	
 	while(1) {
-		bytesRead = read(incoming, &nextInt, 4);
+		sleep(5);
+		bytesRead = read(incoming, &number, 4);
 		if (bytesRead == 0) {
-			printf("%d: 0 bytes read in prime(). Incoming pipe close in prime().\n", getpid());
+			printf("%d: 0 bytes read in prime(). Incoming pipe close in "
+				"prime().\n", getpid()
+			);
+			// write(outgoing, &bytesRead, 4);
 			break;
 		}
 		else if (bytesRead != 4) {
-			printf("%d: Error when reading nextInt.\n", getpid());
+			printf("%d: Error when reading number.\n", getpid());
 			break;
 		}
-		printf("%d: %d read in prime.\n", getpid(), nextInt);
+		printf("%d: %d read in prime.\n", getpid(), number);
 		if (divisor == -1) {
-			divisor = nextInt;
+			divisor = number;
 			continue;
 		}
-		if (nextInt % divisor != 0) {
-			printf("%d: nextInt = %d, divisor = %d, remainder = %d.\n", getpid(), nextInt, divisor, nextInt%divisor);
-			// if (outgoing == -1) {
-			// 	childPid = createProcess(&incoming, &outgoing);
-			// 	if (childPid == -1) {
-			// 		printf("Could not create new process in prime().\n");
-			// 		return;
-			// 	}
-			// 	else if (childPid == 0) {
-			// 		continue;
-			// 	}
-			// }
 
-			// if (write(outgoing, &nextInt, 4) != 4) {
-			// 	printf("Error when writing nextInt.\n");
-			// 	return;
-			// }
+		//if remainder is 0, not a prime so drop the integer
+		if (number % divisor == 0) {
+			printf("%d: %d is a factor of %d\n", 
+				getpid(),
+				number,
+				divisor
+			);
+			continue;
 		}
+		
+		printf("%d: %d is not divisible by %d.\n", 
+			getpid(), 
+			number,
+			divisor
+		);
+		
+		if (outgoing == -1) {
+			printf("%d: No process next in line to handle %d. "
+				"Creating a new process.\n", 
+				getpid(),
+				number
+			);
+			childPid = createProcess(&incoming, &outgoing);
+			sleep(5);
+			if (childPid == -1) {
+				printf("%d: Could not create new process in prime().\n",
+					getpid()
+				);
+				return;
+			}
+			else if (childPid == 0) {
+		sleep(1);
+				
+				printf("Prime %d\n", number);
+				divisor = -1;
+				continue;
+			}
+		}
+
+		sleep(5);
+		if (write(outgoing, &number, 4) != 4) {
+			printf("Error when writing number.\n");
+			return;
+		}
+		
 	}
+	sleep(5);
 	printf("%d: Loop finished in prime(). Now exiting program.\n", getpid());
 	return;
 }
@@ -103,7 +209,7 @@ void prime() {
 void parent(int outgoing) {
 	int nextInt, bytesWritten;
 	nextInt = 1;
-	while (nextInt++ <= MAX_INT) {	
+	while (nextInt++ <= MAX_INT) {
 		printf("\n");
 		printf("%d: Writing %d to child.\n", getpid(), nextInt);
 		if ((bytesWritten = write(outgoing, &nextInt, 4)) != 4) {
@@ -111,7 +217,7 @@ void parent(int outgoing) {
 			printf("%d: Expected to write 4 bytes but instead wrote %d.\n", getpid(), bytesWritten);
 			return;
 		}
-		sleep(1);
+		sleep(30);
 		
 	}
 	printf("\n");
@@ -125,15 +231,27 @@ void parent(int outgoing) {
 int main(int argc, char* argv[]) {
 	int childPid, status, closedPid;
 	
+	pipe1[0] = -1;
+	pipe1[1] = -1;
 	pipe2[0] = -1;
 	pipe2[1] = -1;
 	if (pipe(pipe1) == -1) {
 		printf("%d: Could not create pipe1 pipe.\n", getpid());
 		return 1;
 	}
+	if(close(pipe1[1]) != 0) {
+		printf("%d: Error in main. Could not close pipe1[1]\n", 
+			getpid()
+		);
+		return -1;
+	}
+	pipe1[1] = -1;
+
 	childPid = createProcess();
 	if (childPid == -1) {
-		printf("%d: Could not create new process. Exiting program.\n", getpid());
+		printf("%d: Could not create new process. Exiting program.\n", 
+			getpid()
+		);
 		return 1;
 	}
 	else if (childPid == 0) {
@@ -141,13 +259,14 @@ int main(int argc, char* argv[]) {
 		printf("%d: Returned from prime().\n", getpid());
 	}
 	else {
+		sleep(5);
+		
 		parent(outgoing);
 		while((closedPid = wait(&status) != childPid)) {
 			printf("%d: Process %d closed\n", getpid(), closedPid);
 		}
-		
 	}
-	
+	sleep(10);
 	printf("%d: Exiting program.\n", getpid());
 	return 0;
 }
