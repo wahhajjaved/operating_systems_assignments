62e4bbca1900bda3db0d36f43e8b1d421fc1a96e
Nakhba Mubashir <epl482@usask.ca>
2024-10-03 15:39:40 -0600

    added the design and code for santa and reindeer

--- labs/lab3/Lab3.synchproblem.c
+++ labs/lab3/Lab3.synchproblem.c
@@ -9,18 +9,27 @@
 #include <standards.h>
 #include <os.h>
 
-int elfTex,mutex, santaSem;
+int elfTex,mutex, santaSem, reindeerSem;
 
 int elveNum, reindeerNum;
 
-void getHelp(){
-    printf("getting help");
+void getHelp(void){
+    printf("getting help\n");
+}
+
+void helpElves(void){
+    printf("helpping elves\n");
+}
+
+void prepareSleigh(void){
+    printf("preparing sleigh \n");
+}
+void getHitched(void){
+    printf("getiing hitched \n");
 }
 
 
-
-
-PROCESS Elves(void){
+PROCESS elves(void){
     P(elfTex);
     P(mutex);
 
@@ -37,5 +46,32 @@ PROCESS Elves(void){
     V(mutex);
 }
 
+PROCESS santa(void){
+    int i;
+
+    P(santaSem);
+    P(mutex);
+
+    if (reindeerNum >= 9){
+        prepareSleigh();
+        for (i=0; i<=9; i++){
+            V(reindeerSem);
+            reindeerNum--;
+        }
+    } else if (eleveNum == 3){
+        helpElves();
+    }
+    V(mutex);
+}
+
+PROCESS reindeer(void){
+    P(mutex);
+    reindeerNum++;
+    if (reindeerNum == 9) V(santaSem);
+    V(mutex);
+
+    V(reindeerSem);
+    getHitcth();
+}
 
 
