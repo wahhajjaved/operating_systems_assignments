31322c749d8154e8ab6caba5040847a52ee9aae1
Nakhba Mubashir <epl482@usask.ca>
2024-10-03 20:08:53 -0600

    added the test for lab3 sych, its not giving right output

--- labs/lab3/Lab3.testsynchproblem.c
+++ labs/lab3/Lab3.testsynchproblem.c
@@ -6,10 +6,28 @@
 
 #include <stdio.h>
 #include <stdlib.h>
-#include <standards.h>
+#include <pthread.h>
 #include <os.h>
 
-int main (void){
+#include <standards.h>
 
+
+PROCESS elves(void);
+PROCESS santa(void);
+PROCESS reindeer(void);
+void initialize(void);
+
+int main (void){
+    int i;
+    initialize();
+    /* santa wakes up for elve*/
+    for (i = 0; i  <10; i++) {
+        reindeer();
+    }
+    santa();
+    for (i = 0; i < 4; i++) {
+        elves();
+    }
+    return 0;
 
 }
