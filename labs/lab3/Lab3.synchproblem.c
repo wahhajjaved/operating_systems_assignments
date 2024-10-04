6e3e88d34a972e1c42ee36d5a8fda92920cec8e5
Nakhba Mubashir <epl482@usask.ca>
2024-10-03 20:10:57 -0600

    added the while loop

--- labs/lab3/Lab3.synchproblem.c
+++ labs/lab3/Lab3.synchproblem.c
@@ -6,36 +6,40 @@
 
 #include <stdio.h>
 #include <stdlib.h>
-#include <standards.h>
 #include <os.h>
+#include <standards.h>
+
 
 int elfTex,mutex, santaSem, reindeerSem;
 
-int elveNum=0;
-int reindeerNum=0;
+int elveNum;
+int reindeerNum;
 
 void getHelp(void){
     printf("getting help\n");
+    Sleep(100);
 }
 
 void helpElves(void){
     printf("helpping elves\n");
+    Sleep(100);
 }
 
 void prepareSleigh(void){
     printf("preparing sleigh \n");
+    Sleep(100);
 }
 void getHitched(void){
-    printf("getiing hitched \n");
+    printf(" getiing hitched \n");
+    Sleep(50);
 }
 
 
 PROCESS elves(void){
     P(elfTex);
     P(mutex);
-
     elveNum++;
-    if (eleveNum == 3) V(santaSem);
+    if (elveNum == 3) V(santaSem);
     else V(elfTex);
     V(mutex);
 
@@ -43,26 +47,26 @@ PROCESS elves(void){
 
     P(mutex);
     elveNum--;
-    if (eleveNum == 0) V(elfTex);
+    if (elveNum == 0) V(elfTex);
     V(mutex);
 }
 
 PROCESS santa(void){
     int i;
-
-    P(santaSem);
-    P(mutex);
-
-    if (reindeerNum >= 9){
-        prepareSleigh();
-        for (i=0; i<=9; i++){
-            V(reindeerSem);
-            reindeerNum--;
+    while(1){
+        P(santaSem);
+        P(mutex);
+        if (reindeerNum >= 9){
+            prepareSleigh();
+            for (i=0; i<9; i++){
+                V(reindeerSem);
+                reindeerNum--;
+            }
+        } else if (elveNum == 3){
+            helpElves();
         }
-    } else if (eleveNum == 3){
-        helpElves();
-    }
-    V(mutex);
+        V(mutex);
+        }
 }
 
 PROCESS reindeer(void){
@@ -70,15 +74,16 @@ PROCESS reindeer(void){
     reindeerNum++;
     if (reindeerNum == 9) V(santaSem);
     V(mutex);
-
     V(reindeerSem);
-    getHitcth();
+    getHitched();
 }
 
 void initialize(void){
-    elfTex=NewSem(0);
-    mutex=NewSem(1); 
-    santaSem= NewSem(0);
-    reindeerSem=NewSem(0);
+    elveNum=0;
+    reindeerNum=0;
+    mutex=NewSem(1);  /* mutex */
+    elfTex=NewSem(0); /* allow one elf  to enter*/
+    santaSem= NewSem(1); 
+    reindeerSem=NewSem(1);
 }
 
