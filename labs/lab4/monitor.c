ce59b25fd2eeda73e9c9d94271130f559fe9c013
Wahhaj Javed <muj975@usask.ca>
2024-10-08 11:18:21 -0600

    Stubs for monitor.c and monitor.h created

---
+++ labs/lab4/monitor.c
@@ -0,0 +1,30 @@
+/*
+@author Wahhaj Javed, muj975, 11135711
+@date 2024-10-10
+*/
+
+#include <monitor.h>
+
+void RttMonInit(int numConds){
+    printf("RttMonInit not implemented\n");
+}
+
+void RttMonEnter() {
+    printf("RttMonEnter not implemented\n");
+}
+
+void RttMonLeave(){
+    printf("RttMonLeave not implemented\n");
+}
+
+void RttMonWait(int CV){
+    printf("RttMonWait not implemented\n");
+}
+
+void RttMonSignal(int CV){
+    printf("RttMonSignal not implemented\n");
+}
+
+void MonServer(){
+    printf("MonServer not implemented\n");
+}
---
+++ labs/lab4/monitor.h
@@ -0,0 +1,11 @@
+/*
+@author Wahhaj Javed, muj975, 11135711
+@date 2024-10-10
+*/
+
+void RttMonInit(int numConds);
+void RttMonEnter() ;
+void RttMonLeave();
+void RttMonWait(int CV);
+void RttMonSignal(int CV);
+void MonServer();
