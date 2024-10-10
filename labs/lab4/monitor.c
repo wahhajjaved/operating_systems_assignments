5cff4116e236665764c67051e4682f9ba4dea9c7
Wahhaj Javed <muj975@usask.ca>
2024-10-10 13:54:38 -0600

    RttMonInit implemented

--- labs/lab4/monitor.c
+++ labs/lab4/monitor.c
@@ -3,28 +3,44 @@
 @date 2024-10-10
 */
 
+#include <stdio.h>
 #include <monitor.h>
+#include <list.h>
+
+
+
+LIST* cvLists;
+LIST* enterq;
+LIST* urgentq;
+
 
 void RttMonInit(int numConds){
-    printf("RttMonInit not implemented\n");
+	cvLists = ListCreate();
+	for(numConds; numConds > 0; numConds--) {
+		ListAdd(cvLists, ListCreate());
+	}
+	enterq = ListCreate();
+	urgentq = ListCreate();
+	MonServer();
+	printf("RttMonInit - Lists created and server started.\n");
 }
 
 void RttMonEnter() {
-    printf("RttMonEnter not implemented\n");
+	printf("RttMonEnter not implemented\n");
 }
 
 void RttMonLeave(){
-    printf("RttMonLeave not implemented\n");
+	printf("RttMonLeave not implemented\n");
 }
 
 void RttMonWait(int CV){
-    printf("RttMonWait not implemented\n");
+	printf("RttMonWait not implemented\n");
 }
 
 void RttMonSignal(int CV){
-    printf("RttMonSignal not implemented\n");
+	printf("RttMonSignal not implemented\n");
 }
 
 void MonServer(){
-    printf("MonServer not implemented\n");
+	printf("MonServer not implemented\n");
 }
