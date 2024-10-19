c537da0bdbb6c261532a4e9dbc213b9ccf7fca5d
Wahhaj Javed <muj975@usask.ca>
2024-10-19 17:16:54 -0600

    Input argument parsing complete.

--- assignments/a2_phase2/s-chat.c
+++ assignments/a2_phase2/s-chat.c
@@ -1,11 +1,15 @@
 /*
  *@author Wahhaj Javed, muj975, 11135711
  *@author Nakhba Mubashir, epl482, 11317060
- *@date 2024-10-08
+ *@date 2024-10-24
  */
 
 
 #include <stdio.h>
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <netdb.h>
+#include <arpa/inet.h>
 
 #include <rtthreads.h>
 #include <RttCommon.h>
@@ -33,23 +37,60 @@ RTTTHREAD networkOut() {
 	
 }
 
-int mainp()
+void parseArguments(
+	int argc,
+	char* argv[],
+	uint32_t* src_port,
+	uint32_t* dst_ip,
+	uint32_t* dst_port
+) {
+	int status;
+	struct addrinfo hints, *res;
+	struct sockaddr_in *ipv4;
+
+	if (argc != 4) {
+		printf("Incorrect number of arguments.\n");
+		printf("Usage: s-chat <src_port> <dst_host_name> <dst_port>\n");
+		exit(EXIT_FAILURE);
+	}
+	
+	*src_port = atoi(argv[1]);
+	*dst_port = atoi(argv[3]);
+	
+	
+	memset(&hints, 0, sizeof(hints));
+	hints.ai_family = AF_INET;
+	hints.ai_socktype = SOCK_STREAM;
+	status = getaddrinfo(argv[2], NULL, &hints, &res);
+	if (status != 0) {
+		fprintf(stderr, "getaddrinfo: %s\n", gai_strerror(status));
+		exit(EXIT_FAILURE);
+	}
+	ipv4 = (struct sockaddr_in *)res->ai_addr;
+	*dst_ip = ipv4->sin_addr.s_addr;
+	freeaddrinfo(res);
+}
+
+int mainp(int argc, char* argv[])
 {
-	
 	int temp;
-	
-	RttThreadId serverTid;
-	RttThreadId	consoleInTid;
-	RttThreadId consoleOutTid;
-	RttThreadId networkInTid;
+	uint32_t src_port, dst_ip, dst_port;
+	RttThreadId serverTid, consoleInTid, consoleOutTid, networkInTid;
 	RttThreadId networkOutTid;
+	RttSchAttr attr;
 	
-	RttSchAttr 	attr;
+	parseArguments(argc, argv, &src_port, &dst_ip, &dst_port);
+	printf("Starting s-chat. src port: %u, dst ip: %u, dst port: %u.\n",
+		src_port, 
+		dst_ip, 
+		dst_port
+	);
+	return 0;
+	
+	
 	attr.startingtime = RTTZEROTIME;
 	attr.priority = RTTNORM;
 	attr.deadline = RTTNODEADLINE;
-
-	printf("s-chat starting.\n");
 	setbuf(stdout, 0);
 	
 	temp = RttCreate(
