8ded71473ac50c66e8f8e1657d1292258c6841e3
Nakhba Mubashir <epl482@usask.ca>
2024-10-31 13:59:07 -0600

    added the best fit files

---
+++ labs/lab6/BestFitMonitor.c
@@ -0,0 +1,13 @@
+
+
+BF-init(void){
+
+}
+
+void BF-allocation(int size){
+
+}
+
+void BF-free(int address){
+
+}
---
+++ labs/lab6/BestFitMonitor.h
@@ -0,0 +1,8 @@
+
+
+
+void BF-init(void);
+
+void BF-allocation(int size);
+
+void BF-free(int address);
