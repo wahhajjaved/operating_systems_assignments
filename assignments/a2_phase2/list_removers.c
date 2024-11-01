cd58d84b843ab16602a4c9315be1eb5e3cef17e1
Nakhba Mubashir <epl482@usask.ca>
2024-10-31 21:00:27 -0600

    added the list from phase 2

---
+++ assignments/a2_phase2/list_removers.c
@@ -0,0 +1,259 @@
+/*
+ *@author Wahhaj Javed, muj975, 11135711
+ *@author Nakhba Mubashir, epl482, 11317060
+ *@date 2024-09-16
+ */
+
+#include "list.h"
+
+extern LIST *aviableList;
+extern NODE *aviableNode;
+extern int listNum;
+extern int nodeNum;
+extern LIST *listDict; /* dict of lists*/
+extern NODE *nodeDict; /* dict of all the nodes*/
+
+
+void Decrease_list_memory(){
+    LIST  *newListDict;
+    int Num, i;
+
+    Num= MIN_LISTS / 2;
+    newListDict=realloc(listDict, (Num * sizeof(LIST)));
+    if (newListDict ==NULL){
+            errx(1, "ERROR: MEMORY ALLOCATION FAILED\n");
+            return ;
+    }
+    listDict = newListDict;
+    aviableList = NULL;
+
+    for (i=Num-1; i>=0; i--){
+        if (listDict[i].size == 0){
+            listDict[i].nextfreeList = aviableList;
+            aviableList = &listDict[i];
+
+        }
+    }
+    
+}
+
+void Decrease_node_memory(){
+
+    NODE *newNodeDict;
+    int Num, i;
+    Num= MIN_NODES / 2;
+    if (nodeNum <(MIN_NODES/2)){
+        newNodeDict = realloc (nodeDict, (Num * sizeof(NODE)));
+        if (newNodeDict ==NULL){
+            errx(1, "ERROR: MEMORY ALLOCATION FAILED\n");
+            return ;
+        }
+        
+        nodeDict = newNodeDict;
+        aviableNode = NULL;
+
+        /* rechain the left over nodes */
+        for (i=Num-1; i>=0; i--){
+            nodeDict[i].next = aviableNode;
+            if (i >= 0){
+                nodeDict[i].prev = &nodeDict[i-1];
+            } else {
+                nodeDict[i].prev = NULL;
+            }            
+            aviableNode = &nodeDict[i];
+        }
+    }
+}
+
+
+
+void *ListRemove(LIST *list){
+    NODE *removeNode;
+    void *currItem;
+
+    /* if the List struct pointer is not right */
+    if (list == NULL) { 
+       /* printf("ERROR: list is NULL \n");*/
+        return NULL;
+    }
+
+    /* check if the list is empty or not */
+    if (list-> size ==0){ 
+        /*printf("ERROR: list empty \n");*/
+        return NULL;
+    }
+
+    removeNode= list->curser;
+    currItem= list->curser->item;   
+    if (list->size == 1){
+        list->head = NULL;
+        list->tail = NULL;
+        list->curser = NULL;
+    }
+
+   else  if (list->curser == list->tail){
+        list->tail = list->curser->prev;            
+        removeNode->prev = NULL;
+        list->tail->next = NULL;
+        list->curser =list->tail;
+      
+    }
+    else if (list->curser == list->head){
+        list->head = list->curser->next;
+        list->head->prev = NULL;
+        removeNode->next = NULL;
+        list->curser =list->head;
+        
+    }
+    else {
+        list->curser->prev->next = list->curser->next;
+        list->curser->next->prev = list->curser->prev;
+        list->curser= list->curser->next;
+        removeNode->next= NULL;
+        removeNode->prev= NULL;
+    }
+    list->size--;
+    listNum--;
+
+    removeNode->next= aviableNode;
+    aviableNode = removeNode;
+/*
+    if (listNum < (MIN_LISTS/2)){
+        Decrease_list_memory();
+    } 
+*/
+    return currItem;
+}
+
+void ListFree(LIST *list, ITEMFREE itemFree){
+    NODE *node, *prevNode;
+    void *itemToBeFreed;
+    /* if the List struct pointer is not right
+    */
+    if (list == NULL) {
+        /*printf("ERROR: list or itemfree is NULL \n");*/
+        return ;
+    }
+
+    /* check if the list is empty or not
+    */
+    if (list-> size ==0){
+        prevNode=NULL;
+    }else{
+        prevNode= list->tail;
+    }
+
+    while (prevNode != NULL){
+        node= prevNode;
+        prevNode = node->prev;
+        itemToBeFreed= node->item;
+        (*itemFree)(itemToBeFreed);
+        nodeNum--;
+        node->next = aviableNode;
+        aviableNode = node;
+    }
+
+    list->head = NULL;
+    list->tail = NULL;
+    list->curser = NULL;
+    list->size=0;
+    nodeNum--;
+/*
+    if (listNum < (MIN_LISTS/2)){
+        Decrease_list_memory();
+    } 
+*/
+}
+
+
+void *ListTrim(LIST *list){
+    NODE *removeNode;
+    void *lastItem;
+
+    /* if the List struct pointer is not right */
+    if (list == NULL) { 
+        /*printf( "ERROR: list is NULL \n");*/
+        return NULL;
+    }
+
+    /* check if the list is empty or not */
+    if (list-> size ==0){ 
+        /*printf("ERROR: list empty \n");*/
+        return NULL;
+    }
+
+    removeNode = list->tail;
+    lastItem = list->tail->item;
+    
+    if (list->size == 1){
+        list->head = NULL;
+        list->tail = NULL;
+        list->curser = NULL;
+    }
+    else {
+        list->tail = list->tail->prev;
+        removeNode->prev = NULL;
+        list->tail->next = NULL;
+        list->curser =list->tail;
+        
+    }
+    list->size--;
+    listNum--;
+
+    removeNode->next= aviableNode;
+    aviableNode = removeNode;
+    
+/*
+    if (listNum < (MIN_LISTS/2)){
+        Decrease_list_memory();
+    } 
+*/
+    return lastItem;
+
+}
+
+
+void ListConcat(LIST *list1, LIST *list2){
+     /* if the List struct pointer is not right*/
+    if (list1 == NULL || list2== NULL) {
+       /* printf("ERROR: list1 AND list2 is NULL \n");*/
+        return;
+    }
+    if (list1 == list2) {
+       /* printf("ERROR: IN listConcat, both list are same \n");*/
+        return ;
+    }
+
+    /*list1 is empty and list2 not empty 
+    * concatenate to an empty list */
+    if (list1->size == 0 && list2->size !=0){
+        list1->head = list2->head;
+        list1->tail = list2->tail;
+        list1->curser = list2->curser;
+        list1->size = list2->size;
+    }
+    /* both  list1 and list2 are non empty*/
+    else if (list1->size !=0 && list2->size !=0){
+        list1->tail->next = list2->head;
+        list2->head->prev = list1->tail;
+        list1->tail = list2->tail;
+        list1->size += list2->size;
+    }
+
+    list2->size = 0;
+    list2->head = NULL;
+    list2->tail = NULL;
+    list2->curser = NULL;
+
+    listNum--;
+
+    list2->nextfreeList = aviableList;
+    aviableList = list2;
+/*
+    if (listNum < (MIN_LISTS/2)){
+        Decrease_list_memory();
+    } 
+    */
+}
+
+
