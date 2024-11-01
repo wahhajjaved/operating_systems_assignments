56f129df19b14e9f0a4712749ea4ba325fb8418b
Wahhaj Javed <muj975@usask.ca>
2024-11-01 11:54:07 -0600

    BFAllocate allocated done and tested. Waiting not yet implemented.

--- labs/lab6/best_fit_monitor.c
+++ labs/lab6/best_fit_monitor.c
@@ -7,9 +7,32 @@
 #include <Monitor.h>
 #include <best_fit_monitor.h>
 
-#define TOTALMEMORY 1000
+#define TOTALMEMORY 50
 
 LIST* memory;
+
+
+int compareMemory(void* a, void* b) {
+	return a == b;
+}
+
+void printMemory() {
+	MemoryBlock* m;
+	
+	m = ListFirst(memory);
+	do{
+		printf(
+			"startAddress = %-5d "
+			"endAddress = %-5d "
+			"size = %-5d "
+			"inUse = %-5d \n"
+			,m->startAddress
+			,m->endAddress
+			,m->size
+			,m->inUse
+		);
+	} while((m = ListNext(memory)) != NULL);
+}
 
 void init() {
 	MemoryBlock* m;
@@ -26,8 +49,72 @@ void init() {
 	printf("best fit monitor initialized. List size %d\n", ListCount(memory));
 }
 
+
+
+MemoryBlock* bestFit(int size) {
+	MemoryBlock *m, *bestChunk, *best, *nextChunk;
+	int bestSize;
+	bestSize = TOTALMEMORY+1;
+	
+	/*find the best chunk of free memory*/
+	if((m = ListFirst(memory)) == NULL) {
+		fprintf(stderr, "Error: No memory in memory list.\n");
+		return NULL;
+	}
+	do {
+		int diff;
+		if(m->inUse) continue;
+
+		diff = m->size - size;
+		if(diff >= 0 && diff < bestSize){
+			bestSize = diff;
+			bestChunk = m;
+		}
+	} while((m = ListNext(memory)) != NULL);
+	if(bestSize == TOTALMEMORY+1){
+		printf("Allocating Memory: No memory available.\n");
+		return NULL;
+	}
+	
+	/*split up the best chunk into the exact size needed*/
+	best = malloc(sizeof(MemoryBlock));
+	best->startAddress = bestChunk->startAddress;
+	best->endAddress = bestChunk->startAddress + size - 1;
+	best->size = size;
+	best->inUse = 1;
+	
+	nextChunk = malloc(sizeof(MemoryBlock));
+	nextChunk->startAddress = best->endAddress + 1;
+	nextChunk->endAddress = bestChunk->endAddress;
+	nextChunk->size = nextChunk->endAddress - nextChunk->startAddress + 1;
+	nextChunk->inUse = 0;
+	
+	ListFirst(memory);
+	ListSearch(memory, compareMemory, bestChunk);
+	ListRemove(memory);
+	ListAdd(memory, best);
+	ListAdd(memory, nextChunk);
+	
+	
+	return best;
+	
+}
+
 int* BFAllocate(int size){
-	MemoryBlock* m = ListFirst(memory);
+	MemoryBlock* m;
+	
+	printf("\n");
+	printf("\n------------------------------------------------------------\n");
+	printf("Allocating %d Memory: Current Memory Layout\n", size);
+	printMemory();
+	
+	m = bestFit(size);
+	
+	printf("------\n");
+	printf("Allocated %d Memory: New Memory Layout\n", size);
+	printMemory();
+	printf("------------------------------------------------------------\n");
+	
 	return &m->startAddress;
 }
 
