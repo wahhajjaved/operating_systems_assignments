97d706e5ed742fcbea760d5e2bff65a03042271c
Nakhba Mubashir <epl482@usask.ca>
2024-11-12 21:52:00 -0600

    edited the vm.c for uvmcopy

--- assignments/a3/xv6-riscv/kernel/vm.c
+++ assignments/a3/xv6-riscv/kernel/vm.c
@@ -315,28 +315,39 @@ uvmcopy(pagetable_t old, pagetable_t new, uint64 sz)
   pte_t *pte;
   uint64 pa, i;
   uint flags;
-  char *mem;
+/*  char *mem;*/
 
   for(i = 0; i < sz; i += PGSIZE){
     if((pte = walk(old, i, 0)) == 0)
       panic("uvmcopy: pte should exist");
     if((*pte & PTE_V) == 0)
       panic("uvmcopy: page not present");
+    /*CMPT 332 GROUP 67 Change, Fall 2024 */
+    *pte &= ~PTE_W;  /*set to read only*/
+    
     pa = PTE2PA(*pte);
     flags = PTE_FLAGS(*pte);
-    if((mem = kalloc()) == 0)
-      goto err;
-    memmove(mem, (char*)pa, PGSIZE);
-    if(mappages(new, i, PGSIZE, (uint64)mem, flags) != 0){
-      kfree(mem);
-      goto err;
-    }
-  }
-  return 0;
 
+    incriRefCount(pa);
+
+    /*if((mem = kalloc()) == 0)
+      goto err;
+    memmove(mem, (char*)pa, PGSIZE);*/
+
+    if(mappages(new, i, PGSIZE, (uint64)mem, flags) != 0){
+      return -1;
+     /*kfree(mem);
+      goto err;*/
+    }
+    
+  }
+
+  return 0;
+/*
  err:
   uvmunmap(new, 0, i / PGSIZE, 1);
   return -1;
+*/
 }
 
 /* mark a PTE invalid for user access. */
