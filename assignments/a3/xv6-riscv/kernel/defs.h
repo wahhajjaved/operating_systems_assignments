1f283ea5a9c0d31672fa313e4a4fe03190a564ce
Wahhaj Javed (muj975) <2321-muj975@users.noreply.git.cs.usask.ca>
2024-11-13 20:18:05 -0600

    Revert "groupsinit() declaration added so it can be used by main()"
    This reverts commit 8c2e0a02bc6ff53409c7ccb86685c183adc58e4c

--- assignments/a3/xv6-riscv/kernel/defs.h
+++ assignments/a3/xv6-riscv/kernel/defs.h
@@ -96,7 +96,6 @@ struct cpu*     mycpu(void);
 struct cpu*     getmycpu(void);
 struct proc*    myproc();
 void            procinit(void);
-void            groupsinit();
 void            scheduler(void) __attribute__((noreturn));
 void            sched(void);
 void            sleep(void*, struct spinlock*);
