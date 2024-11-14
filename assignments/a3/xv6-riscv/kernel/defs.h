c522d32653db4f670f5f1325ab59673e0cbf981c
Wahhaj Javed <wahhajjaved@gmail.com>
2024-11-13 20:19:04 -0600

    groupsinit() declaration added so it can be used by main()

--- assignments/a3/xv6-riscv/kernel/defs.h
+++ assignments/a3/xv6-riscv/kernel/defs.h
@@ -96,6 +96,7 @@ struct cpu*     mycpu(void);
 struct cpu*     getmycpu(void);
 struct proc*    myproc();
 void            procinit(void);
+void            groupsinit();
 void            scheduler(void) __attribute__((noreturn));
 void            sched(void);
 void            sleep(void*, struct spinlock*);
