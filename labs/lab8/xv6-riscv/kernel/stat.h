b67a66e1c92315564be607f59b924e90e02cf4ad
Nakhba Mubashir <epl482@usask.ca>
2024-11-21 12:59:52 -0600

    added the new file type for symblink

--- labs/lab8/xv6-riscv/kernel/stat.h
+++ labs/lab8/xv6-riscv/kernel/stat.h
@@ -1,6 +1,8 @@
 #define T_DIR     1   /* Directory */
 #define T_FILE    2   /* File */
 #define T_DEVICE  3   /* Device */
+/*NAKHBA MUBASHIR lab 08 11317060 epl482*/
+#define T_SYMLINK  4   /* symlink */
 
 struct stat {
   int dev;     /* File system's disk device */
