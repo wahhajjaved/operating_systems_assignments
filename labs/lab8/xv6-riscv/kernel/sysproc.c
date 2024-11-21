9d580ad283bfc1c60d0134f11d9657bd4f3ecf6d
Nakhba Mubashir <epl482@usask.ca>
2024-11-21 11:47:56 -0600

    lab8 started

---
+++ labs/lab8/xv6-riscv/kernel/sysproc.c
@@ -0,0 +1,155 @@
+#include "types.h"
+#include "riscv.h"
+#include "defs.h"
+#include "param.h"
+#include "memlayout.h"
+#include "spinlock.h"
+#include "proc.h"
+
+uint64
+sys_exit(void)
+{
+  int n;
+  argint(0, &n);
+  exit(n);
+  return 0;  /* not reached */
+}
+
+uint64
+sys_getpid(void)
+{
+  return myproc()->pid;
+}
+
+uint64
+sys_fork(void)
+{
+  return fork();
+}
+
+uint64
+sys_wait(void)
+{
+  uint64 p;
+  argaddr(0, &p);
+  return wait(p);
+}
+
+uint64
+sys_sbrk(void)
+{
+  uint64 addr;
+  int n;
+
+  argint(0, &n);
+  addr = myproc()->sz;
+  if(growproc(n) < 0)
+    return -1;
+  return addr;
+}
+
+uint64
+sys_sleep(void)
+{
+  int n;
+  uint ticks0;
+
+  argint(0, &n);
+  if(n < 0)
+    n = 0;
+  acquire(&tickslock);
+  ticks0 = ticks;
+  while(ticks - ticks0 < n){
+    if(killed(myproc())){
+      release(&tickslock);
+      return -1;
+    }
+    sleep(&ticks, &tickslock);
+  }
+  release(&tickslock);
+  return 0;
+}
+
+uint64
+sys_kill(void)
+{
+  int pid;
+
+  argint(0, &pid);
+  return kill(pid);
+}
+
+/* return how many clock tick interrupts have occurred */
+/* since start. */
+uint64
+sys_uptime(void)
+{
+  uint xticks;
+
+  acquire(&tickslock);
+  xticks = ticks;
+  release(&tickslock);
+  return xticks;
+}
+
+uint64
+sys_trace(void)
+{
+    int mask; /* to hols trace mask*/
+    struct proc *p =myproc();
+    argint(0, &mask); /* aquire the mask*/
+    acquire (&p->lock);
+    p->traceMask = mask; /*set the trace mask*/
+    release (&p->lock);
+    return 0;
+}
+/* CMPT 332 GROUP 67 Change, Fall 2024 A3 */
+uint64 sys_getNumFreePages(void){
+    return getNumFreePages();
+}
+uint64
+sys_procdump(void)
+{
+  procdump();
+  return 0;
+}
+
+uint64
+sys_setprocessgroup(void)
+{
+  int pid, groupnumber;
+  argint(0, &pid);
+  argint(1, &groupnumber);
+
+  return setprocessgroup(pid, groupnumber);
+
+
+
+}
+
+uint64
+sys_setshare(void)
+{
+  int groupnumber, newshare, remainingshares, r1, r2;
+  uint64 premainingshares;
+
+  argint(0, &groupnumber);
+  argint(1, &newshare);
+  argaddr(2, &premainingshares);
+
+  r1 = setshare(groupnumber, newshare, &remainingshares);
+  r2 = copyout(
+        myproc()->pagetable,
+        premainingshares,
+        (char *)&remainingshares,
+        sizeof(int)
+      );
+
+  if(r1 == 0 && r2 == 0)
+    return 0;
+
+  return -1;
+
+}
+
+/* **********************************  */
