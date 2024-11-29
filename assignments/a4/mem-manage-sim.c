61a0e8bef4c1ae4a89f29e26fcf5ea0169a48cbf
Nakhba Mubashir <epl482@usask.ca>
2024-11-29 09:06:33 -0600

    wdited malloc test and main

--- assignments/a4/mem-manage-sim.c
+++ assignments/a4/mem-manage-sim.c
@@ -15,15 +15,15 @@
 #define ALLOC_STDEV 0xC000     /* 48 KiB */
 #define SLEEP_MEAN  10000     /* 10 ms */
 
-int allocSizes[MAX_THREADS][MAX_ITERATIONS];
-int sleepTimes[MAX_THREADS][MAX_ITERATIONS];
 bool doFree[MAX_THREADS][MAX_ITERATIONS];
+int allocSizes[MAX_THREADS][MAX_ITERATIONS];
+int sleepTime[MAX_THREADS][MAX_ITERATIONS];
 
 
 typedef struct _params {
 	int threadNo;
 	int algNo;
-} PARAMS;
+} PARAM;
 
 int numIterations;
 int numThreads;
@@ -50,7 +50,7 @@ int mainp(int argc, char* argv[]) {
 
 	RttSchAttr attr;
 	RttThreadId pid[NUM_ALGS][MAX_THREADS];
-	PARAMS* params;
+	PARAM* params;
 
 	attr.startingtime = RTTZEROTIME;
 	attr.priority = RTTNORM;
@@ -90,26 +90,25 @@ int mainp(int argc, char* argv[]) {
 			else {
 				allocSizes[i][j] = ALLOC_MEAN - ALLOC_STDEV * x;
                 /* make sure allocation is positive */
-				if (allocSizes[i][j] <= 0)  
-					allocSizes[i][j] = 1;
+				if (allocSizes[i][j] <= 0) allocSizes[i][j] = 1;
 			}
 
-			/*exponential distribution for sleep times */
+			/*exp distribution for sleep times */
 			u = drand48();
-			sleepTimes[i][j] = - SLEEP_MEAN * log(u);
+			sleepTime[i][j] = - SLEEP_MEAN * log(u);
 
-			/* random binary value for free */
+			/* random value for free */
 			doFree[i][j] = drand48() > 0.5;
 		}
 	}	
 
-	Initialize(numThreads);  /* initialize the monitor */
-	/* Create the threads for each algorithm */
+	Initialize(numThreads);  /* initializefor BF and FF */
+	/* Create the threads for FF and BF */
 	for (j = 0; j < numThreads; j++) {
 		for (i = 0; i < NUM_ALGS; i++) {
-			params = malloc(sizeof(PARAMS));
+			params = malloc(sizeof(PARAM));
+            params->algNo = i;
 			params->threadNo = j;
-			params->algNo = i;
 
 			temp = RttCreate(&pid[i][j], (void(*)()) MallocTest, STKSIZE, NULL, 
 				 	params, attr, RTTUSR);
@@ -126,24 +125,23 @@ int mainp(int argc, char* argv[]) {
 
 
 RTTTHREAD MallocTest(void *arg) {
-	int i;
-	int algNo, threadNo;
+	int i, algNo, threadNo, freeIndex;
 	void *allocatedAddrs[MAX_ITERATIONS];
 
-	threadNo = ((PARAMS*) arg)->threadNo;
-	algNo = ((PARAMS*) arg)->algNo;
-	free(arg);
+	threadNo = ((PARAM*) arg)->threadNo;
+	algNo = ((PARAM*) arg)->algNo;
+	/*free(arg);*/
 	printf("threadNo=%d, algNo=%d\n", threadNo, algNo); 
 
 	/* Initialize the allocated address array to all 0s (NULL) */
 	for (i = 0; i < numIterations; i++) allocatedAddrs[i] = NULL;
 
-	/* Main simulation loop */
+	/* Main loop */
 	for (i = 0; i < numIterations; i++) {
 /*		printf("%d-%d: attempting to allocate %u bytes\n", algNo, threadNo,
 				allocSizes[threadNo][i]); 
 */
-		/* Allocate a block of memory */
+		/* Allocate memory */
 		allocatedAddrs[i] = MyMalloc(allocSizes[threadNo][i], algNo);
 /*		printf("%d-%d: allocated block: %p to %p\n", algNo, threadNo, 
 				(void*) allocatedAddrs[i], 
@@ -151,28 +149,23 @@ RTTTHREAD MallocTest(void *arg) {
 				- 1)); 
 */
 		/* Sleep for some time */
-		RttUSleep(sleepTimes[threadNo][i]);
-/*		printf("%d-%d: slept for %d ticks\n", algNo, threadNo,
-				sleepTimes[threadNo][i]); 
-*/
-		/* If freeing memory this iteration... */
-		if (doFree[threadNo][i]) {
-			int freeIndex;
-			
-			/* Determine a random addr to free from the random alloc size */
-			freeIndex = allocSizes[threadNo][i] % (i + 1);
+		RttUSleep(sleepTime[threadNo][i]);
 
+		/* freeing memory this */
+		if (doFree[threadNo][i]) {
+			/* determine a random addr to free from the random alloc size */
+			freeIndex = allocSizes[threadNo][i] % (i + 1);
 			/* Linear probing if address has already been freed */
 			while (allocatedAddrs[freeIndex] == NULL)
 				freeIndex = (freeIndex + 1) % (i + 1);	
 
 			MyFree(allocatedAddrs[freeIndex], algNo);
-/*	printf("%d-%d: freed block of allocated memory at 0x%lx\n", 
+            /*	printf("%d-%d: freed block of allocated memory at 0x%lx\n", 
 				algNo, threadNo, (long) allocatedAddrs[freeIndex]); 
 			allocatedAddrs[freeIndex] = NULL;
 */	}
 	}
 
-	Threadend(algNo);  /* Prints results if this is the last thread */
+	Threadend(algNo);  /* Print the results if its the last thread */
 }
 
