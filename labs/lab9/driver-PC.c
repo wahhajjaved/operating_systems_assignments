6cafda3caf24c10cd887d5d081b77fdbc9311070
Wahhaj Javed <muj975@usask.ca>
2024-12-05 14:23:22 -0600

    Single character tests added

--- labs/lab9/driver-PC.c
+++ labs/lab9/driver-PC.c
@@ -15,33 +15,71 @@ int writeEnd;
 
 void test1() {
     int buffSize, bytes_written, bytes_read;
-    char writeChars[10] = "aabbccddee";
-    char readChars[10];
-    buffSize = 10;
+    char writeChar = 'a';
+    char readChar;
+    buffSize = 1;
 
-    bytes_written = write(writeEnd, writeChars, buffSize);
+    bytes_written = write(writeEnd, &writeChar, buffSize);
     if (bytes_written != buffSize) {
         printf("test1: Expected to write %d bytes but only wrote %d\n",
         buffSize, bytes_written);
     }
 
-    bytes_read = read(readEnd, readChars, buffSize);
+    bytes_read = read(readEnd, &readChar, buffSize);
     if (bytes_read != buffSize) {
-        printf("test1: Expected to read %d bytes but only read %d\n",
-        buffSize, bytes_read);
+        printf(
+            "test1: Expected to read %d bytes but only read %d. "
+            "readChars = %c\n",
+            buffSize, bytes_read, readChar
+        );
     }
 
-    if (bytes_read == buffSize) {
-        int i;
-        for(i = 0; i < buffSize; i++) {
-            if (writeChars[i] != readChars[i]) {
-                printf(
-                    "test1: characters at position %d are different. "
-                    "writeChars = %s, readChars = %s",
-                    i, writeChars, readChars
-                );
-            }
-        }
+    if (bytes_read == buffSize && readChar == writeChar) {
+        printf("test1: wrote %c and read %c. test1 passed.\n",
+                writeChar, readChar
+        );
+    }
+    else {
+        printf(
+            "test1: characters different. "
+            "writeChar = %c, readChar = %c\n",
+            writeChar, readChar
+        );
+    }
+}
+
+void test2() {
+    int buffSize, bytes_written, bytes_read;
+    char writeChar = '2';
+    char readChar;
+    buffSize = 1;
+
+    bytes_written = write(writeEnd, &writeChar, buffSize);
+    if (bytes_written != buffSize) {
+        printf("test2: Expected to write %d bytes but only wrote %d\n",
+        buffSize, bytes_written);
+    }
+
+    bytes_read = read(readEnd, &readChar, buffSize);
+    if (bytes_read != buffSize) {
+        printf(
+            "test2: Expected to read %d bytes but only read %d. "
+            "readChars = %c\n",
+            buffSize, bytes_read, readChar
+        );
+    }
+
+    if (bytes_read == buffSize && readChar == writeChar) {
+        printf("test2: wrote %c and read %c. test2 passed.\n",
+                writeChar, readChar
+        );
+    }
+    else {
+        printf(
+            "test2: characters different. "
+            "writeChar = %c, readChar = %c\n",
+            writeChar, readChar
+        );
     }
 }
 
@@ -49,14 +87,18 @@ void test1() {
 int main(int argc, char* argv[]) {
 
     readEnd = open("/dev/vfifofum1", O_RDONLY);
+    if (readEnd == -1){
+        perror("could not open /dev/vfifofum1");
+        return 1;
+    }
     writeEnd = open("/dev/vfifofum0", O_WRONLY);
-
-    if (readEnd == -1)
-        errx(1, "could not open /dev/vfifofum1");
-    if (writeEnd == -1)
-        errx(1, "could not open /dev/vfifofum0");
+    if (writeEnd == -1){
+        perror("could not open /dev/vfifofum0");
+        return 1;
+    }
 
     test1();
+    test2();
 
     close(readEnd);
     close(writeEnd);
